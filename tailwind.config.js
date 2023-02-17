f6cef9b99568b0a8b314b12a2dcb3d661ce65d46
Robby Pambudi <5025211042@mhs.its.ac.id>
2023-02-17 18:35:32 +0700

    style: add color, font and images

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,28 @@ module.exports = {
       fontFamily: {
         primary: ['Inter', ...fontFamily.sans],
       },
+      colors: {
+        primary: {
+          100: '#D5FEF1',
+          200: '#B9FDE8',
+          300: '#96FBDD',
+          400: '#73FAD2',
+          500: '#50F9C6',
+          600: '#2DF8BB',
+          700: '#1EA57D',
+          800: '#177C5E',
+        },
+        secondary: {
+          100: '#D3D8DE',
+          200: '#B6BDC8',
+          300: '#919CAC',
+          400: '#6D7C91',
+          500: '#495B76',
+          600: '#243A5A',
+          700: '#1E304B',
+          800: '#18273C',
+        },
+      },
     },
   },
   plugins: [require('@tailwindcss/forms')],
